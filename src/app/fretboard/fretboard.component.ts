db768d907344175e118d31542a420bd9289f97b6
Peter Guntermann <pgu@posteo.de>
2020-01-26 16:56:27 +0100

    Refactor

--- src/app/fretboard/fretboard.component.ts
+++ src/app/fretboard/fretboard.component.ts
@@ -11,24 +11,36 @@ export class FretboardComponent implements OnInit {
     @Input() numberOfFrets = 15;
     guitarStrings: number[];
     frets: number[];
-    fretNumbers: string[];
+    fretboardLabels: string[];
 
     ngOnInit() {
-      this.guitarStrings = [...Array(this.numberOfStrings).keys()].map(f => f + 1).reverse();
-      this.frets = [...Array(this.numberOfFrets).keys()].map(f => f + 1);
-      this.fretNumbers = this.frets.map(f=>{
-        switch (f) {
-          case 3: return 'III';
-          case 5: return 'V';
-          case 7: return 'VII';
-          case 9: return 'IX';
-          case 12: return 'XII';
-          case 15: return 'XV';
-          case 17: return 'XVII';
-          case 19: return 'IXX';
-          default: return '';
-        }
-      });
+        this.guitarStrings = [...Array(this.numberOfStrings).keys()].map(f => f + 1).reverse();
+        this.frets = [...Array(this.numberOfFrets).keys()].map(f => f + 1);
+        this.fretboardLabels = this.getFretboardLabels();
     }
 
+    private getFretboardLabels() {
+        return this.frets.map(fret => {
+            switch (fret) {
+                case 3:
+                    return 'III';
+                case 5:
+                    return 'V';
+                case 7:
+                    return 'VII';
+                case 9:
+                    return 'IX';
+                case 12:
+                    return 'XII';
+                case 15:
+                    return 'XV';
+                case 17:
+                    return 'XVII';
+                case 19:
+                    return 'IXX';
+                default:
+                    return '';
+            }
+        });
+    }
 }
