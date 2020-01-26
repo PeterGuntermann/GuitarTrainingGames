a730693c6e2600f80269a4bae36695a9c172f16c
Peter Guntermann <guntermann.peter@gmail.com>
2020-01-26 14:10:37 +0100

    Extract components for `header` and `content-box`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent }        from './app.component';
+import { ContentBoxComponent } from './content-box/content-box.component';
+import { HeaderComponent }     from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ContentBoxComponent,
+        HeaderComponent,
+    ],
+    imports: [
+        BrowserModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/content-box/content-box.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-content-box',
+    templateUrl: './content-box.component.html',
+    styleUrls: ['./content-box.component.scss'],
+})
+export class ContentBoxComponent implements OnInit {
+
+    @Input() label: string;
+    @Input() value: string;
+
+    ngOnInit() {
+    }
+
+}
