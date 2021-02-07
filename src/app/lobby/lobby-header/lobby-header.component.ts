99d93cc7861e2a6c2718821d84170f8a3cbe2ace
Peter Guntermann <pguntermann@andrena.de>
2021-02-08 00:51:26 +0100

    Implement lobby-header

--- src/app/lobby/lobby-header/lobby-header.component.ts
+++ src/app/lobby/lobby-header/lobby-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { faEllipsisH, faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
     selector: "lobby-header",
@@ -6,7 +7,16 @@ import { Component, Input, OnInit } from "@angular/core";
     styleUrls: ["./lobby-header.component.scss"],
 })
 export class LobbyHeaderComponent implements OnInit {
+    readonly faEllipsisV = faEllipsisV;
+    readonly faEllipsisH = faEllipsisH;
+
+    public isMenuOpened = false;
+
     @Input() title: string;
 
     ngOnInit() {}
+
+    toogleMenu(): void {
+        this.isMenuOpened = !this.isMenuOpened;
+    }
 }
--- src/app/lobby/lobby.module.ts
+++ src/app/lobby/lobby.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { SharedModule } from "../shared/shared.module";
 import { LobbyHeaderComponent } from "./lobby-header/lobby-header.component";
 import { LobbyLayoutComponent } from "./lobby-layout/lobby-layout.component";
 
 @NgModule({
     declarations: [LobbyHeaderComponent, LobbyLayoutComponent],
-    imports: [CommonModule, SharedModule],
+    imports: [CommonModule, FontAwesomeModule, SharedModule],
     exports: [LobbyLayoutComponent],
 })
 export class LobbyModule {}
