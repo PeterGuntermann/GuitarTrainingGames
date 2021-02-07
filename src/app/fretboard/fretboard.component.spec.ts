b66705ceb8e36db156e19498a22788f39e6c8ba9
Peter Guntermann <pgu@posteo.de>
2021-02-07 15:16:55 +0100

    Update to Angular 11

--- src/app/fretboard/fretboard.component.spec.ts
+++ src/app/fretboard/fretboard.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FretboardComponent } from './fretboard.component';
 
@@ -6,7 +6,7 @@ describe('FretboardComponent', () => {
   let component: FretboardComponent;
   let fixture: ComponentFixture<FretboardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FretboardComponent ]
     })
