0cfe09dfbf2d61e72aa47dfe4cdcccef092ec154
Peter Guntermann <pguntermann@andrena.de>
2021-02-08 00:33:42 +0100

    Consistent folder renaming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
-import { LobbyLayoutComponent } from "./lobby/layout/lobby-layout.component";
+import { LobbyLayoutComponent } from "./lobby/lobby-layout/lobby-layout.component";
 import { LobbyModule } from "./lobby/lobby.module";
 import { RoomLayoutComponent } from "./room/room-layout/room-layout.component";
 import { RoomModule } from "./room/room.module";
--- src/app/lobby/lobby.module.ts
+++ src/app/lobby/lobby.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared/shared.module";
-import { LobbyHeaderComponent } from "./header/lobby-header.component";
-import { LobbyLayoutComponent } from "./layout/lobby-layout.component";
+import { LobbyHeaderComponent } from "./lobby-header/lobby-header.component";
+import { LobbyLayoutComponent } from "./lobby-layout/lobby-layout.component";
 
 @NgModule({
     declarations: [LobbyHeaderComponent, LobbyLayoutComponent],
