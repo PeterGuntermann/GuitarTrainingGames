9464a118b0d52219ab770129927f6dd9d9a706a4
Peter Guntermann <pguntermann@andrena.de>
2021-02-11 21:03:49 +0100

    Implement lobby-header "the material way"

--- src/app/lobby/lobby.module.ts
+++ src/app/lobby/lobby.module.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { SharedModule } from "../shared/shared.module";
 import { LobbyHeaderComponent } from "./lobby-header/lobby-header.component";
@@ -7,7 +10,14 @@ import { LobbyLayoutComponent } from "./lobby-layout/lobby-layout.component";
 
 @NgModule({
     declarations: [LobbyHeaderComponent, LobbyLayoutComponent],
-    imports: [CommonModule, FontAwesomeModule, SharedModule],
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+        SharedModule,
+        MatButtonModule,
+        MatIconModule,
+        MatToolbarModule,
+    ],
     exports: [LobbyLayoutComponent],
 })
 export class LobbyModule {}
