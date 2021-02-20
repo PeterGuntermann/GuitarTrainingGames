7a9e993daa7e3893e39a761b769a79955dccddc7
Peter Guntermann <pgu@posteo.de>
2021-02-20 17:43:37 +0100

    Improve sidenav menu

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import {
+    GAME_CHORDS_ROOM,
+    GAME_NOTES_ROOM,
+    GAME_SCALES_ROOM,
+} from "../constants/game-rooms";
 import { APP_NAME, APP_VERSION } from "../constants/globals";
 
 @Component({
@@ -7,6 +12,9 @@ import { APP_NAME, APP_VERSION } from "../constants/globals";
     styleUrls: ["./sidenav.component.scss"],
 })
 export class SidenavComponent implements OnInit {
+    readonly GAME_CHORDS_ROOM = GAME_CHORDS_ROOM;
+    readonly GAME_NOTES_ROOM = GAME_NOTES_ROOM;
+    readonly GAME_SCALES_ROOM = GAME_SCALES_ROOM;
     readonly VERSION_INFO = `${APP_NAME} v${APP_VERSION}`;
 
     ngOnInit(): void {}
