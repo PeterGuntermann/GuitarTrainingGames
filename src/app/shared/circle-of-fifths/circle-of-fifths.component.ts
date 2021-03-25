1a3559b52ff2927de1f4a9761aa9c2d380634f26
Peter Guntermann <pgu@posteo.de>
2021-03-25 21:18:59 +0100

    Display image of the current note

--- src/app/shared/circle-of-fifths/circle-of-fifths.component.ts
+++ src/app/shared/circle-of-fifths/circle-of-fifths.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Note } from "../../models/note.enum";
 
 @Component({
     selector: "app-circle-of-fifths",
@@ -6,9 +7,55 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./circle-of-fifths.component.scss"],
 })
 export class CircleOfFifthsComponent implements OnInit {
+    displayedNote: Note;
     imgSource = "assets/images/notes/Ab.png";
+
+    get imageSource(): string {
+        let filename: string;
+        // TODO: 25.03.2021 Use early returns
+        switch (this.displayedNote) {
+            case Note.C:
+                filename = "C";
+                break;
+            case Note.Csharp_Dflat:
+                filename = "Db";
+                break;
+            case Note.D:
+                filename = "D";
+                break;
+            case Note.Dsharp_Eflat:
+                filename = "Eb";
+                break;
+            case Note.E:
+                filename = "E";
+                break;
+            case Note.F:
+                filename = "F";
+                break;
+            case Note.Fsharp_Gflat:
+                filename = "Gb";
+                break;
+            case Note.G:
+                filename = "G";
+                break;
+            case Note.Gsharp_Aflat:
+                filename = "Ab";
+                break;
+            case Note.Asharp_Bflat:
+                filename = "Bb";
+                break;
+            case Note.B:
+                filename = "B";
+                break;
+            case Note.A:
+                filename = "A";
+        }
+        return `assets/images/notes/${filename}.png`;
+    }
 
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.displayedNote = Note.Fsharp_Gflat;
+    }
 }
