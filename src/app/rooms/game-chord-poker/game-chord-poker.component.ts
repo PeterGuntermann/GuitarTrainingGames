b488d75bff69e94e6ead6e2492063f3ffc76b5b5
Peter Guntermann <pgu@posteo.de>
2021-04-02 20:50:53 +0200

    Move "How to play" for Chord Poker and Scale Machine into dialog

--- src/app/rooms/game-chord-poker/game-chord-poker.component.ts
+++ src/app/rooms/game-chord-poker/game-chord-poker.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from "@angular/core";
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { GAME_CHORD_POKER_ROOM } from "@constants/game-rooms";
 import { ChordType } from "@models/chord-type.enum";
 import { Note } from "@models/note.enum";
 import { RandomGeneratorService } from "@services/random-generator.service";
+import {
+    HelpDialogComponent,
+    HelpDialogData,
+} from "../../shared/help-dialog/help-dialog.component";
 
 interface RollResult {
     rootNote: Note;
@@ -16,10 +21,18 @@ interface RollResult {
 })
 export class GameChordPokerComponent implements OnInit {
     readonly ROOM = GAME_CHORD_POKER_ROOM;
+    readonly HOW_TO_PLAY_STEPS: string[] = [
+        "Click the button below to roll a random chord.",
+        "Find at least one way to play that chord.",
+        "Re-roll to get another chord.",
+    ];
 
     rollResult: RollResult;
 
-    constructor(private _randomGenerator: RandomGeneratorService) {}
+    constructor(
+        private _randomGenerator: RandomGeneratorService,
+        readonly dialog: MatDialog
+    ) {}
 
     ngOnInit(): void {}
 
@@ -28,5 +41,11 @@ export class GameChordPokerComponent implements OnInit {
             rootNote: this._randomGenerator.randomNote(),
             chordType: this._randomGenerator.randomChordType(),
         };
+    }
+
+    onOpenHelpClick(): void {
+        const dialogData: HelpDialogData = { howToPlaySteps: this.HOW_TO_PLAY_STEPS };
+        const dialogConfig: MatDialogConfig = { data: dialogData };
+        this.dialog.open(HelpDialogComponent, dialogConfig);
     }
 }
--- src/app/rooms/game-scale-machine/game-scale-machine.component.ts
+++ src/app/rooms/game-scale-machine/game-scale-machine.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from "@angular/core";
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { GAME_SCALE_MACHINE_ROOM } from "@constants/game-rooms";
 import { GuitarString } from "@models/guitar-string.enum";
 import { Note } from "@models/note.enum";
 import { Scale } from "@models/scale.enum";
 import { RandomGeneratorService } from "@services/random-generator.service";
+import {
+    HelpDialogComponent,
+    HelpDialogData,
+} from "../../shared/help-dialog/help-dialog.component";
 
 interface RollResult {
     rootNote: Note;
@@ -18,18 +23,32 @@ interface RollResult {
 })
 export class GameScaleMachineComponent implements OnInit {
     readonly ROOM = GAME_SCALE_MACHINE_ROOM;
+    readonly HOW_TO_PLAY_STEPS: string[] = [
+        "Click the button below to roll a mode, a root node and a string to start on.",
+        "Play the first 9 notes of that mode up and down again.",
+        "Re-roll to get another mode.",
+    ];
 
     rollResult: RollResult;
 
-    constructor(readonly randomGenerator: RandomGeneratorService) {}
+    constructor(
+        readonly randomGenerator: RandomGeneratorService,
+        readonly dialog: MatDialog
+    ) {}
 
     ngOnInit(): void {}
 
-    roll() {
+    roll(): void {
         this.rollResult = {
             rootNote: this.randomGenerator.randomNote(),
             scale: this.randomGenerator.randomScale(),
             guitarString: this.randomGenerator.randomGuitarStringForScales(),
         };
+    }
+
+    onOpenHelpClick(): void {
+        const dialogData: HelpDialogData = { howToPlaySteps: this.HOW_TO_PLAY_STEPS };
+        const dialogConfig: MatDialogConfig = { data: dialogData };
+        this.dialog.open(HelpDialogComponent, dialogConfig);
     }
 }
