79bb1189f706f4cc9cf50576e53d84b738a7c0ba
Peter Guntermann <pgu@posteo.de>
2021-04-06 21:55:32 +0200

    Fix compile error

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,6 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { CircleOfFifthsComponent } from "./circle-of-fifths/circle-of-fifths.component";
-import { ContentBoxComponent } from "./content-box/content-box.component";
 import { FretboardComponent } from "./fretboard/fretboard.component";
 import { HelpDialogComponent } from "./help-dialog/help-dialog.component";
 import { RollButtonComponent } from "./roll-button/roll-button.component";
@@ -12,7 +11,6 @@ import { ValueDisplayComponent } from "./value-display/value-display.component";
 
 const components = [
     CircleOfFifthsComponent,
-    ContentBoxComponent,
     FretboardComponent,
     HelpDialogComponent,
     RollButtonComponent,
